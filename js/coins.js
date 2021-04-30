554b8ac750ff75e0ad27a8a8664e42d6149cd381
Kelvin Mendes de Araújo <65052645+kelvin-had1@users.noreply.github.com>
2021-04-30 16:20:50 -0300

    first commit

---
+++ js/coins.js
@@ -0,0 +1,34 @@
+const url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-USD"
+    let request = new XMLHttpRequest()
+
+    const div = document.querySelector('.coin')
+    
+    
+
+    
+
+
+    request.open('GET', url, true)
+    request.onreadystatechange = function(){
+        let response = JSON.parse(request.responseText)
+        let USDBRL = parseFloat(response['USDBRL']['bid']).toFixed(2).replace(',','.')
+        
+        let USDTime = new Date(response['USDBRL']['timestamp']*1000)
+        let hours = USDTime.getHours()
+        let minutes = USDTime.getMinutes()
+        let seconds = USDTime.getSeconds()
+        let USDHorario = `${hours}:${minutes}:${seconds}`
+        
+        
+
+        let contentdiv = `<h1>Dólar</h1>
+        <p>Dólar em real: <br> R$ ${USDBRL}</p>
+        <p>Horário da informação: <br> ${USDHorario} - ${USDTime.toLocaleDateString("pt-BR")}</p>`
+
+        div.innerHTML = contentdiv
+}
+
+
+
+request.send()
+    
---
+++ js/crypto.js
@@ -0,0 +1,42 @@
+const APICrypto = 'https://www.mercadobitcoin.net/api/BTC/ticker'
+const divCrypto = document.querySelector('.crypto')
+let bitcoin = {}
+
+let requestCrypto = new XMLHttpRequest()
+requestCrypto.open('GET', APICrypto, true)
+
+
+requestCrypto.onreadystatechange = function(){
+    let BTC = JSON.parse(requestCrypto.responseText)
+    bitcoin.high = BTC['ticker']['high'].slice(0, -6).replace('.', ',')
+    bitcoin.low = BTC['ticker']['low'].slice(0, -6).replace('.', ',')
+    bitcoin.last = BTC['ticker']['last'].slice(0, -6).replace('.', ',')
+    bitcoin.dateUnix = BTC['ticker']['date']
+
+
+    let BTCdate = new Date(bitcoin.dateUnix*1000)
+    let hours = BTCdate.getHours()
+    let minutes = BTCdate.getMinutes()
+    let seconds = BTCdate.getSeconds()
+    let BTCTime = `${hours}:${minutes}:${seconds}`
+    
+    
+    const cryptoContent = `<h1>Bitcoin</h1>
+    <p>Menor preço:<br> R$ ${bitcoin.low}</p>
+    <p>Maior preço:<br> R$ ${bitcoin.high}</p>
+    <p>Valor da última negociação:<br> R$ ${bitcoin.last}</p>
+    <p>Horário da informação: <br> ${BTCTime} - ${BTCdate.toLocaleDateString("pt-BR")}
+    </p>`
+   
+
+    divCrypto.innerHTML = cryptoContent
+
+ 
+    
+    
+}
+requestCrypto.send()
+
+
+
+
